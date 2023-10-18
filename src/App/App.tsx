717f924c3a2780e504ed9511d46c9fd638dadbd5
Dima <57713733+Dima1409@users.noreply.github.com>
2023-10-18 08:35:03 +0300

    add suspense

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,4 +1,4 @@
-import { lazy } from "react";
+import { lazy, Suspense, ReactNode } from "react";
 import Container from "components/Container";
 import { Route, Routes, Navigate } from "react-router-dom";
 
@@ -7,14 +7,41 @@ const ExpensesPage = lazy(() => import("../pages/Expenses"));
 const IncomePage = lazy(() => import("../pages/Income"));
 const TransfersPage = lazy(() => import("../pages/Transfers"));
 
+const LoadingFallback: React.FC = () => <div>Loading...</div>;
+
+const LazyPage: React.FC<{ children: ReactNode }> = ({ children }) => (
+  <Suspense fallback={<LoadingFallback />}>{children}</Suspense>
+);
+
 function App() {
   return (
     <Container>
       <Routes>
         <Route path="/" element={<HomePage />}></Route>
-        <Route path="/income" element={<IncomePage />}></Route>
-        <Route path="/expenses" element={<ExpensesPage />}></Route>
-        <Route path="/transfers" element={<TransfersPage />}></Route>
+        <Route
+          path="/income"
+          element={
+            <LazyPage>
+              <IncomePage />
+            </LazyPage>
+          }
+        ></Route>
+        <Route
+          path="/expenses"
+          element={
+            <LazyPage>
+              <ExpensesPage />
+            </LazyPage>
+          }
+        ></Route>
+        <Route
+          path="/transfers"
+          element={
+            <LazyPage>
+              <TransfersPage />
+            </LazyPage>
+          }
+        ></Route>
         <Route path="*" element={<Navigate to="/" replace />}></Route>
       </Routes>
     </Container>
