73335df6c07e90e8bf166f3bd02d5bd43ed948cb
Dima <57713733+Dima1409@users.noreply.github.com>
2023-12-12 22:57:24 +0200

    sharedLayout

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -2,14 +2,16 @@ import { lazy, Suspense, ReactNode, useEffect } from "react";
 import Container from "components/Container";
 import Loader from "components/Loader";
 import { Route, Routes, Navigate } from "react-router-dom";
-import { refreshUser } from "../redux/auth/operations";
-import { useDispatch } from "react-redux";
+// import { refreshUser } from "../redux/auth/operations";
+// import { useDispatch } from "react-redux";
 import Routs from "components/Routs";
-import Hooks from "hooks";
+import SharedLayout from "components/SharedLayout";
+import LoginPage from "../pages/LoginPage";
+import HomePage from "../pages/HomePage";
 
-const HomePage = lazy(() => import("../pages/HomePage"));
+// const HomePage = lazy(() => import("../pages/HomePage"));
 const RegisterPage = lazy(() => import("../pages/RegisterPage"));
-const LoginPage = lazy(() => import("../pages/LoginPage"));
+// const LoginPage = lazy(() => import("../pages/LoginPage"));
 const ExpensesPage = lazy(() => import("../pages/Expenses"));
 const IncomePage = lazy(() => import("../pages/Income"));
 const TransfersPage = lazy(() => import("../pages/Transfers"));
@@ -21,73 +23,50 @@ const LazyPage: React.FC<{ children: ReactNode }> = ({ children }) => (
 );
 
 const App: React.FC = () => {
-  const dispatch = useDispatch();
-  const { isRefreshing } = Hooks.useAuth();
-  useEffect(() => {
-    dispatch(refreshUser() as any);
-  }, [dispatch]);
+  // const dispatch = useDispatch();
+  // useEffect(() => {
+  //   dispatch(refreshUser() as any);
+  // }, [dispatch]);
   return (
     <Container>
-      {isRefreshing ? (
-        <div>Refreshing...</div>
-      ) : (
-        <Routes>
+      <Routes>
+        <Route path="/" element={<SharedLayout />}>
+          <Route index={true} element={<HomePage />} />
           <Route
-            path="/"
+            path="/register"
+            element={
+              <Routs.RestrictedRoute
+                component={RegisterPage}
+                redirectTo="/income"
+              />
+            }
+          ></Route>
+          <Route path="/login" element={<LoginPage />}></Route>
+          <Route
+            path="/income"
+            element={
+              <Routs.PrivateRoute component={IncomePage} redirectTo="/login" />
+            }
+          ></Route>
+          <Route
+            path="/expenses"
             element={
               <LazyPage>
-                {/* <HomePage /> */}
-                <LoginPage></LoginPage>
+                <ExpensesPage />
               </LazyPage>
             }
-          >
-            <Route
-              path="/register"
-              element={
-                <Routs.RestrictedRoute
-                  component={RegisterPage}
-                  redirectTo="/income"
-                />
-              }
-            ></Route>
-            <Route
-              path="/login"
-              element={
-                <Routs.RestrictedRoute
-                  component={LoginPage}
-                  redirectTo="/income"
-                />
-              }
-            ></Route>
-            <Route
-              path="/income"
-              element={
-                <Routs.PrivateRoute
-                  component={IncomePage}
-                  redirectTo="/login"
-                />
-              }
-            ></Route>
-            <Route
-              path="/expenses"
-              element={
-                <LazyPage>
-                  <ExpensesPage />
-                </LazyPage>
-              }
-            ></Route>
-            <Route
-              path="/transfers"
-              element={
-                <LazyPage>
-                  <TransfersPage />
-                </LazyPage>
-              }
-            ></Route>
-            <Route path="*" element={<Navigate to="/" replace />}></Route>
-          </Route>
-        </Routes>
-      )}
+          ></Route>
+          <Route
+            path="/transfers"
+            element={
+              <LazyPage>
+                <TransfersPage />
+              </LazyPage>
+            }
+          ></Route>
+          <Route path="*" element={<Navigate to="/" replace />}></Route>
+        </Route>
+      </Routes>
     </Container>
   );
 };
---
+++ src/components/SharedLayout/SharedLayout.tsx
@@ -0,0 +1,11 @@
+const SharedLayout: React.FC = () => {
+  return (
+    <>
+      <div>Welcome to myMoney</div>
+      <div>Name</div>
+      <div>Avatar</div>
+    </>
+  );
+};
+
+export default SharedLayout;
