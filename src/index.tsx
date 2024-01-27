21b1cf6e89b48dd8714f00d124ba2b9570a81f25
Dima <57713733+Dima1409@users.noreply.github.com>
2024-01-27 21:46:36 +0200

    basename

--- src/index.tsx
+++ src/index.tsx
@@ -14,7 +14,7 @@ root.render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter basename="/my_money">
+        <BrowserRouter basename="">
           <App />
         </BrowserRouter>
       </PersistGate>
