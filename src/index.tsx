24a7c2a73c315ae7fb2ecc002f8cf601d331fc88
Dima <57713733+Dima1409@users.noreply.github.com>
2024-01-28 22:59:16 +0200

    fix router

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,27 @@
+// import React from "react";
+// import ReactDOM from "react-dom/client";
+// import { Provider } from "react-redux";
+// import { store, persistor } from "./redux/store";
+// import { PersistGate } from "redux-persist/integration/react";
+// import "./index.css";
+// import App from "./App/App";
+// import { BrowserRouter } from "react-router-dom";
+
+// const root = ReactDOM.createRoot(
+//   document.getElementById("root") as HTMLElement
+// );
+// root.render(
+//   <React.StrictMode>
+//     <Provider store={store}>
+//       <PersistGate loading={null} persistor={persistor}>
+//         <BrowserRouter basename="/my-money">
+//           <App />
+//         </BrowserRouter>
+//       </PersistGate>
+//     </Provider>
+//   </React.StrictMode>
+// );
+
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
@@ -5,18 +29,19 @@ import { store, persistor } from "./redux/store";
 import { PersistGate } from "redux-persist/integration/react";
 import "./index.css";
 import App from "./App/App";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
+
 root.render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter basename="/my-money">
+        <HashRouter>
           <App />
-        </BrowserRouter>
+        </HashRouter>
       </PersistGate>
     </Provider>
   </React.StrictMode>
